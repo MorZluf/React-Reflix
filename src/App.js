c2888fb6aeeb00edc0d1c55b31911b38f5966a46
MorZluf <morzluf19@gmail.com>
2019-05-06 18:17:23 +0300

    first route to home and links to catalog and home + some basic css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react';
-import './App.css';
-// import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import React, { Component } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Landing from './components/Landing'
 
 class App extends Component {
   constructor() {
     super()
-    this.state = {
-    }
+    // this.state = {
+    // }
 
   }
 
   render() {
     return (
-      <div>hello</div>
+      <Router>
+        <div className="App">
+          <div id="main-links">
+            <span id="home-link"><Link to="/">Home</Link></span>
+            <span id="catalog-link"><Link to="/catalog">Catalog</Link></span>
+            <span id="reflix-logo">REFLIX</span>
+          </div>
+          <Route path="/" exact component={Landing} />
+
+
+
+
+
+
+        </div>
+
+      </Router>
     )
 
-      
+
 
 
   }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
-class Landing extends Component{
+class Landing extends Component {
 
-    render(){
+    render() {
 
-        return
+        return (
+            <div>
+                <h3>Who's Watching ?</h3>
+                <div id="users-container">
+                    <div className="user" id="user1"><Link to="/catalog">Rick</Link></div>
+                    <div className="user" id="user2"><Link to="/catalog">Morty</Link></div>
+                    <div className="user" id="user3"><Link to="/catalog">Summer</Link></div>
+                    <div className="user" id="user4"><Link to="/catalog">Jerry</Link></div>
+                </div>
+            </div>
+        )
 
 
-        
+
     }
 }
 
